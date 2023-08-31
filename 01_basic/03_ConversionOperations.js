b6116edc1466f6b6e20b27172674ecff42117508
Shikhar Khandelwal <73191595+skykunnu@users.noreply.github.com>
2023-08-31 16:53:07 +0000

    conversion

--- 01_basic/03_ConversionOperations.js
+++ 01_basic/03_ConversionOperations.js
@@ -1,17 +1,17 @@
 let score="Shikhar"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 let valueInNumber=Number(score)
-console.log(valueInNumber); // Nan- Not a number. 
+// console.log(valueInNumber); // Nan- Not a number. 
 
-// "33"=> 33
-// "33abc"=>NaN
-// true=>1; false=>0
+// // "33"=> 33
+// // "33abc"=>NaN
+// // true=>1; false=>0
 
 let isLoggedIn ="Shikhar"
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1=> true; 0=> false
 // "" => false
@@ -19,5 +19,11 @@ console.log(booleanIsLoggedIn);
 
 let someNumber=33
 let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ******************Operations*********************
+let value=3
+let negValue=-value
+//console.log(negValue);
+
