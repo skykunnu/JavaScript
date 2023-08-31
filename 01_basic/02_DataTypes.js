f6e315739fe7dad1ce17557ef686968135a01b22
Shikhar Khandelwal <73191595+skykunnu@users.noreply.github.com>
2023-08-31 16:28:11 +0000

    datatypes

---
+++ 01_basic/02_DataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS version as newer version
+// alert(3+3) // we are using nodejs, not browser
+
+let name="Shikhar"
+let age=18
+let isLoggedIn=false
+
+//number => 2 to power 53
+//bigint
+// string=> ""
+//boolean => true/false
+//null => standalone value/empty value.
+//undefined => no value is assigned. 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
