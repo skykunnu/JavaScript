b54d04146c7a5de7a93252d7ec112576cc6ecd7c
Shikhar Khandelwal <73191595+skykunnu@users.noreply.github.com>
2023-08-31 15:49:26 +0000

    config control done

--- 01_basic/01_Variables.js
+++ 01_basic/01_Variables.js
@@ -1,2 +1,12 @@
-const add_01="1234";
-console.log(add_01);
+const account_Id="1234"
+let accountEmail="shikhar@gmail.com"
+var accountPassword="1234"
+accountCity="Jaipur" // not to use it. but this is also a way to declare a variable. 
+console.log(accountEmail); // 
+
+
+accountEmail="khandelwal@gmail.com"
+accountPassword="4455"
+accountCity="Bangalore"
+let accountName; // output will be undefined because there is no value assigned to it.
+console.table([account_Id,accountCity,accountEmail,accountPassword,accountName]) // it will output all the items tabular manner. 
