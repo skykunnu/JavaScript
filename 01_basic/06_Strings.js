4a67d24261e818a978191c3418d6519cb03e9380
Shikhar Khandelwal <73191595+skykunnu@users.noreply.github.com>
2023-09-11 19:21:06 +0000

    commit

--- 01_basic/06_Strings.js
+++ 01_basic/06_Strings.js
@@ -3,11 +3,26 @@ const repocount=10
 
 // console.log(name + repocount);
 
-// console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+// console.log(`Hello my name is ${name} and my repo count is ${repocount}`); // use of Template literals.
 
-const gameName=new String('Shikhar Khandelwal')
+const gameName=new String('Shikhar-Jagdish-Khandelwal') // 
 // console.log(gameName.length);
 // console.log(typeof gameName);
 // console.log(gameName.toLowerCase());
-console.log(gameName.charAt(5));
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('a'));
 
+
+const newString=gameName.substring(0, 4)
+// console.log(newString);
+
+const anotherString=gameName.slice(2,4) // it will print the value from (a,b-1).
+// console.log(anotherString);
+// const newStringOne="   Shikhar   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim()); 
+
+const url="https://flipkart.in";
+// console.log(url.replace('in','com')) // it is like replace this from this.
+// console.log(url.includes('Shikhar')) // To Check whether the string  or something is present in variable or not. 
+// console.log(gameName.split('-')); // change the dash with array.  
