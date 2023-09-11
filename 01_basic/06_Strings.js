defacacce3804c6af6e042737623bb87cf44cbd9
Shikhar Khandelwal <73191595+skykunnu@users.noreply.github.com>
2023-09-11 18:54:12 +0000

    commit

--- 01_basic/06_Strings.js
+++ 01_basic/06_Strings.js
@@ -3,4 +3,11 @@ const repocount=10
 
 // console.log(name + repocount);
 
-console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+// console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+
+const gameName=new String('Shikhar Khandelwal')
+// console.log(gameName.length);
+// console.log(typeof gameName);
+// console.log(gameName.toLowerCase());
+console.log(gameName.charAt(5));
+
