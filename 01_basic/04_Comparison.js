2e06397dd27d601ee48e018e2a26251f8bc8ceb3
Shikhar Khandelwal <73191595+skykunnu@users.noreply.github.com>
2023-09-11 10:25:42 +0000

    commit

--- 01_basic/04_Comparison.js
+++ 01_basic/04_Comparison.js
@@ -1,4 +1,4 @@
-// console.log("2">1); // "2" is converted by Javascript into a number that is why the output is true. 
+// console.log("2">1); // "2" is converted to a number by JS, that is why the output is true. 
 // console.log(null>0);
 // console.log(null==0);
 // console.log(null>=0); 
@@ -7,8 +7,8 @@ comparisons convert null to a number, treating it as 0
 that's why null >=0 is true and null>0 is false.
 */
 
-// console.log(undefined==0);
-// console.log(undefined>0);
-// console.log(undefined<0);
+// console.log(undefined==0); false
+// console.log(undefined>0); false
+// console.log(undefined<0); false
 
 //===
