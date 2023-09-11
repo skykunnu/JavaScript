72b8cc9173e94582c834621e2cbc7d4421be6ea7
Shikhar Khandelwal <73191595+skykunnu@users.noreply.github.com>
2023-09-11 10:23:09 +0000

    commit

--- 01_basic/04_Comparison.js
+++ 01_basic/04_Comparison.js
@@ -1,8 +1,8 @@
-// console.log("2">1);
+// console.log("2">1); // "2" is converted by Javascript into a number that is why the output is true. 
 // console.log(null>0);
 // console.log(null==0);
 // console.log(null>=0); 
-/*The reason is that an equality check == and comparison >< >= <= work differently.
+/*The reason is that an equality check == and comparison >< >= <= works differently.
 comparisons convert null to a number, treating it as 0
 that's why null >=0 is true and null>0 is false.
 */
