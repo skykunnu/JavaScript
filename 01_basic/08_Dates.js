25544b3986f1145d0be2d5b824bf750b368c8d3e
Shikhar Khandelwal <73191595+skykunnu@users.noreply.github.com>
2023-09-13 14:36:49 +0000

    commit

---
+++ 01_basic/08_Dates.js
@@ -0,0 +1,23 @@
+let myDate=new Date()
+// following below are Date Stamps.
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+
+let myCreatedDate=new Date(2023,0,23) // (YY,MM,DD) and also In JS Month starts from 0. 
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
+let CreateDate=new Date("01-12-2023")
+// console.log(CreateDate.toLocaleString());
+
+let myTimeStamp=Date.now()
+// console.log(myTimeStamp); // result comes in millimeter. 
+// console.log(Math.floor(Date.now()/1000)); // convert into seconds. 
+
+let newDate=new Date().toLocaleString('default', { 
+    weekday: "long",
+}) // define the date according to your Customization. 
