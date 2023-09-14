fa1d48008a60f04f47bec5bab73769096b6440e4
Shikhar Khandelwal <73191595+skykunnu@users.noreply.github.com>
2023-09-14 11:41:59 +0000

    commit

--- 02_basic/03_objects.js
+++ 02_basic/03_objects.js
@@ -1,1 +1,16 @@
-  
+// singleton
+
+//   1. whenever we declare object with literals then no singleton is formed.
+//   2. when declare with constructor then singleton is formed. 
+//  object.create- declaration of object using constructor method. 
+
+// object literals
+
+
+
+const JsUser={
+    name: "Shikhar",
+    
+
+}
+
