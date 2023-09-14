f990db87cca27fb022d783283fc0abdca4804831
Shikhar Khandelwal <73191595+skykunnu@users.noreply.github.com>
2023-09-14 05:32:33 +0000

    commit

--- 02_basic/02_arrays.js
+++ 02_basic/02_arrays.js
@@ -4,5 +4,16 @@
 //  console.log(marvel_heroes);
 //  console.log(marvel_heroes[3][1]);
 
-marvel_heroes.concat(dc)
-console.log(marvel_heroes);
+// concat function add two arrays and return them into a new array. 
+// const allHeroes=marvel_heroes.concat(dc)
+// console.log(allHeroes);
+
+// spread operator(...)- another way to add two different arrays into one. 
+const all_new_heroes=[...marvel_heroes,...dc] 
+// console.log(all_new_heroes);
+
+// flat( depth- means upto how many lines you want your array to be printed.) function combines all the subarray into one and return it to a new array. 
+const another_array=[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+const real_another_array=another_array.flat(1)
+console.log(real_another_array);
+
