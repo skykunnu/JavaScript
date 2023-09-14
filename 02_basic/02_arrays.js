fbfe025936e6f4ce4b1b166c4a1bb9cbb7975281
Shikhar Khandelwal <73191595+skykunnu@users.noreply.github.com>
2023-09-14 11:17:27 +0000

    commit

--- 02_basic/02_arrays.js
+++ 02_basic/02_arrays.js
@@ -15,5 +15,13 @@ const all_new_heroes=[...marvel_heroes,...dc]
 // flat( depth- means upto how many lines you want your array to be printed.) function combines all the subarray into one and return it to a new array. 
 const another_array=[1,2,3,[4,5,6],7,[6,7,[4,5]]]
 const real_another_array=another_array.flat(1)
-console.log(real_another_array);
+// console.log(real_another_array);
 
+console.log(Array.isArray("Shikhar"))
+console.log(Array.from(["Shikhar","Khandelwal"])) // convert anything like Strings,object into an array. 
+console.log(Array.from({name: "Shikhar"})); // it will return an empty array.As it is not able to convert it.  
+
+let score1=100;
+let score2=200;
+let score3=300;
+console.log(Array.of(score1,score2,score3));
