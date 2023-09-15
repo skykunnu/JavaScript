6cee199c60e1b261a4ea4d1986f4fc6e3fbddad8
Shikhar Khandelwal <73191595+skykunnu@users.noreply.github.com>
2023-09-15 18:11:46 +0000

    commit

--- 02_basic/04_objects.js
+++ 02_basic/04_objects.js
@@ -22,7 +22,8 @@ const regularUser={   // object nested in another object.
 const obj1={1: "a", 2: "b"}
 const obj2={3: "a", 4: "b"}
 
-const obj3={ obj1, obj2}
+// const obj3={ obj1, obj2}
+const obj3=Object.assign(obj1,obj2)
 console.log(obj3);
 
 
