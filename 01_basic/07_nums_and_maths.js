5dce00222c28e619dfc83e4525c8ea6669a2bd2f
Shikhar Khandelwal <73191595+skykunnu@users.noreply.github.com>
2023-10-01 14:15:13 +0530

    07_nums_and_maths.js

--- 01_basic/07_nums_and_maths.js
+++ 01_basic/07_nums_and_maths.js
@@ -9,7 +9,7 @@ const balance=new Number(100)
 
 const otherNumber=128.4966
 //The funda behind the precision is that start the count from extreme left. like for Ex the precision is 3 , so will start count from left and stop after the count of 3 and will check the value after count is greater than or equal to 5 or less than 5. if more will add +1 to previous value and if less will return as it.  
-// console.log(otherNumber.toPrecision(6));
+// console.log(otherNumber.toPrecision(6)); // 128.497
 
 const hundreds=1000000
 // console.log(hundreds.toLocaleString('en-IN')); // conversion to Indian Standard.
