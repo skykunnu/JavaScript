9f00d0b75afaa76ec72705af681c6ffff32a8431
Shikhar Khandelwal <skykunnu@gmail.com>
2023-12-04 23:13:20 +0530

    new

--- 01_basic/02_DataTypes.js
+++ 01_basic/02_DataTypes.js
@@ -1,19 +1,19 @@
 "use strict"; //treat all JS version as newer version
 // alert(3+3) // we are using nodejs, not browser
 
-let name="Shikhar"
-let age=18
-let isLoggedIn=false
+let name = "Shikhar";
+let age = 18;
+let isLoggedIn = false;
 
 //number => 2 to power 53
 //bigint
 // string=> ""
 //boolean => true/false
 //null => standalone value/empty value.
-//undefined => no value is assigned. 
+//undefined => no value is assigned.
 // symbol => unique
 
 // object
 
-console.log(typeof undefined) // undefined
-console.log(typeof null) // object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object, it is a type of bug.
