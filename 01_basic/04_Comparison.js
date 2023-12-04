124929a4a3b7d694393747c40793a29f4f26c38b
Shikhar Khandelwal <skykunnu@gmail.com>
2023-12-04 23:35:11 +0530

    new

--- 01_basic/04_Comparison.js
+++ 01_basic/04_Comparison.js
@@ -1,7 +1,7 @@
-// console.log("2">1); // "2" is converted to a number by JS due to type coercion, which is why the output is true. 
+// console.log("2">1); // "2" is converted to a number by JS due to type coercion, which is why the output is true.
 // console.log(null>0);
 // console.log(null==0);
-// console.log(null>=0); 
+// console.log(null>=0);
 /*The reason is that an equality check == and comparison >< >= <= works differently.
 comparisons convert null to a number, treating it as 0
 that's why null >=0 is true and null>0 is false.
@@ -12,5 +12,9 @@ that's why null >=0 is true and null>0 is false.
 // console.log(undefined<0); false
 
 //=== (Strict check)
-console.log("2"== 2); //true because equality operator change string to a number. 
-console.log("2"===2); //false because strict equal don't change the datatype. 
+console.log("2" == 2); //true because equality operator only see the value if they are equal it will return true or else false.
+console.log("2" === 2); //false because strict equal don't change the datatype.
+
+// != vs !==
+console.log("7" != 7); // false, because as both of them have same values. No matter whatever the datatype is.
+console.log("7" !== 7); // true, because it checked the datatypes of both the value and found it different.
