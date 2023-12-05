0d2fcc57814c7e490e9d53f4a64e8278de7a5a5d
Shikhar Khandelwal <skykunnu@gmail.com>
2023-12-05 20:08:19 +0530

    new

--- 02_basic/01_arrays.js
+++ 02_basic/01_arrays.js
@@ -1,44 +1,29 @@
-const myArr=[0,1,2,3,4]
-const myHeros=["Hulk","Captain America"]
+const myArr = [0, 1, 2, 3, 4];
+const myHeros = ["Hulk", "Captain America"];
 
-const myArr1=new Array(1,2,3,4)
+const myArr1 = new Array(1, 2, 3, 4);
 console.log(myArr[0]);
+console.log(Array.isArray(myArr)); // checks whether a variable is array or not. because if you do the type of myArr it will give an object in return not the array.
 // Array Methods
 
-myArr.push(6) // add the element from the end. 
-myArr.pop() // remove the element from the end. 
+myArr.push(6); // add the element from the end.
+myArr.pop(); // remove the element from the end.
 
 myArr.unshift(9); // add the element from the start.
-myArr.shift() // remove the element from  the start. 
+myArr.shift(); // remove the element from  the start.
 
 console.log(myArr.includes(3)); // returns boolean value based on the existence of an element.
 
-const newArr=myArr.join() // converts the array into string rest is all same. 
+const newArr = myArr.join(); // converts the array into string rest all is same.
 
-
-// slice and splice. 
+// slice and splice.
 
 console.log("A ", myArr);
 
-const myn1=myArr.slice(1,3) // prints the index value 1 and 2 not 3. It does'nt modify the original array.  
+const myn1 = myArr.slice(1, 3); // prints the index value 1 and 2 not 3. It does'nt modify the original array.
 console.log(myn1);
 
 const originalArray = [1, 2, 3, 4, 5];
 originalArray.splice(1, 2); // Removes 2 elements starting at index 1: [1, 4, 5]
 originalArray.splice(2, 0, 6, 7); // Inserts 6 and 7 at index 2: [1, 4, 6, 7, 5]
-// Splice modifies the original array. 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Splice modifies the original array.
