9fd68fc539749c53d5fe7698ff8eee64583837a3
Shikhar Khandelwal <skykunnu@gmail.com>
2023-12-06 00:43:16 +0530

    new

--- 02_basic/03_objects.js
+++ 02_basic/03_objects.js
@@ -1,45 +1,46 @@
+/*
+Objects- (Reference type or Non-Primitive)
+-> Objects store key value pair.
+-> Objects don't have index
+*/
+
 // singleton
 
 //   1. whenever we declare object with literals then no singleton is formed.
-//   2. when declare with constructor then singleton is formed. 
-//  object.create- declaration of object using constructor method. 
+//   2. when declare with constructor then singleton is formed.
+//  object.create- declaration of object using constructor method.
 
 // object literals
 
-const mySym=Symbol("key1")
+const mySym = Symbol("key1");
 
-
-const JsUser={
-    name: "Shikhar",
-    "full name":"Shikhar Khandelwal",
-    [mySym]:"myKey1", // to use it as symbol put the variable in square brackets. 
-    age:22,
-    Location:"Jaipur",
-    Email:"shikharkhandelwal27@gmail.com",
-    isLoggedIn: false,
-    LastLoginDays:["Monday","Saturday"]
+const JsUser = {
+  name: "Shikhar",
+  "full name": "Shikhar Khandelwal",
+  [mySym]: "myKey1", // to use it as symbol put the variable in square brackets.
+  age: 22,
+  Location: "Jaipur",
+  Email: "shikharkhandelwal27@gmail.com",
+  isLoggedIn: false,
+  LastLoginDays: ["Monday", "Saturday"],
 };
 
 // console.log(JsUser.Email);
 // console.log(JsUser["full name"]);
 // console.log(JsUser[mySym]);
 
-// to overwrite the values of an object 
-JsUser.Email="shikhar@gmail.com"
-// Object.freeze(JsUser ) // It freezes all the keys of an object. means you cannot change the keys of object. 
-JsUser.Email="Shikhar25@gmail.com"
+// to overwrite the values of an object
+JsUser.Email = "shikhar@gmail.com";
+// Object.freeze(JsUser ) // It freezes all the keys of an object. means you cannot change the keys of object.
+JsUser.Email = "Shikhar25@gmail.com";
 // console.log(JsUser.Email);
 
-JsUser.greeting=function(){
-    console.log("Hello JS Users.");
-}
-JsUser.greetingTwo=function(){
-    console.log(`Hello JS Users, ${this.name}`);
-}
+JsUser.greeting = function () {
+  console.log("Hello JS Users.");
+};
+JsUser.greetingTwo = function () {
+  console.log(`Hello JS Users, ${this.name}`);
+};
 
 // console.log(JsUser.greeting());
 // console.log(JsUser.greetingTwo());
-
-
-
-
