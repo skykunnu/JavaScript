c6b6a3ace42ae6dea4d8c60d3f738f6d0b7247ad
Shikhar Khandelwal <skykunnu@gmail.com>
2023-12-06 14:56:55 +0530

    new

--- 02_basic/03_objects.js
+++ 02_basic/03_objects.js
@@ -18,12 +18,21 @@ const person = {
 // console.log(person.hobbies);--> ["Playing Cricket", "Listening to music", "Reading about GK"]--> it is a dot notation.
 // or console.log(person["hobbies"])--> it is a bracket notation
 /* How to add a new key value pair to objects */
-person["gender"] = "Male"; // or Person.gender="Male".
-console.log(person); // Whole elements of the object will be printed out.
+// person["gender"] = "Male"; // or Person.gender="Male".
+// console.log(person); // Whole elements of the object will be printed out.
 
 /* Difference between dot and bracket notation. */
-const person = {
+const key = "email";
+const Person = {
   name: "Shikhar",
   age: 22,
-  hobbies: ["Playing Cricket", "Listening to music", "Reading about GK"],
+  "Person hobbies": [
+    "Playing Cricket",
+    "Listening to music",
+    "Reading about GK",
+  ],
 };
+/* How to insert */
+Person[key] = "shikhar@gmail.com";
+console.log(Person);
+console.log(Person["Person hobbies"]); // you cannot access variables separated by space with dot notation.
