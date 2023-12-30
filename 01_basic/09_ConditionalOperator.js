17fc9904010211682be0c5dac12e6932306cf872
Shikhar Khandelwal <skykunnu@gmail.com>
2023-12-30 16:49:28 +0530

    new

---
+++ 01_basic/09_ConditionalOperator.js
@@ -0,0 +1,20 @@
+// Ternary Operator
+/*
+---------------------------
+if-else Operator
+---------------------------
+let age = 4;
+let drink;
+if (age >= 5) {
+  drink = "coffee";
+} else {
+  drink = "Milk";
+}
+console.log(drink);
+*/
+
+/* Structure of ternary operator
+let age = 3;
+let drink = age >= 5 ? "coffee" : "milk";
+console.log(drink);
+*/
