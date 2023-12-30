1081adbff5b515a83082987c60f3111427641baf
Shikhar Khandelwal <skykunnu@gmail.com>
2023-12-31 01:57:09 +0530

    new

--- 02_Arrays_Objects/01_arrays.js
+++ 02_Arrays_Objects/01_arrays.js
@@ -55,11 +55,11 @@ for (let fruit of fruits) {
   // format --> for(let (anyname-for ex fruit,vegetables etc) of {arrayname-> only name with which the array is initialized.}). --> ()-> means it can contain any name. {}-> means only name of the array will be used.
   fruit2.push(fruit);
 }
-console.log(fruit2);
+console.log(fruit2); // ["Apple", "Orange", "banana", "Guava"];
 
 // =================================================================================================================
 
-// For in loop in array
+// For in loop in array -> (Always returns the index of an element).
 const cars = ["BMW", "Mercedes", "Audi", "Rolls Royce"];
 const cars2 = [];
 for (let car in cars) {
