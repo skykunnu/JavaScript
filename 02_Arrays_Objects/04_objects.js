7177a0bc7dc565158bdd70cff483b514e9af7539
Shikhar Khandelwal <skykunnu@gmail.com>
2023-12-31 16:40:33 +0530

    new

--- 02_Arrays_Objects/04_objects.js
+++ 02_Arrays_Objects/04_objects.js
@@ -44,3 +44,44 @@ const obj2 = {
 
 const newObject = { ...obj1, ...obj2, key6: "value-6" }; // new key is added along with objects.
 // console.log(newObject);
+//---------------------------------------------------------------------------------------------------------
+
+/* Object Destructuring */
+const band = {
+  bandName: "led zepplin",
+  famousSong: "stairway to heaven",
+  year: 2010,
+  AnotherFamousSong: "Zara Zara",
+};
+
+let { bandName, famousSong, ...Rest } = band;
+// console.log(bandName);
+
+// Nested Destructuring
+const Users = [
+  { userId: 1, firstName: "Pant", Gender: "Male" },
+  { userId: 2, firstName: "Prakhar", Gender: "Male" },
+  { userId: 3, firstName: "Harshit", Gender: "Female" },
+  { userId: 4, firstName: "Himanshu", Gender: "Male" },
+  { userId: 5, firstName: "Yash", Gender: "Male" },
+];
+
+const [user1, user2, user3, user4, user5] = Users; // these user1,2 and so on are the indexes containg objects.
+// console.log(user1);
+const [{ firstName }, , { Gender }] = Users; // here we have rename the 0th index, 2nd index and remove the 1st index.
+// this will print the values of 0th index firstName and 2nd index Gender.
+// let say if you want to change the name of firstName to userFirstName, Simply use FirstName: userFirstName.
+console.log(firstName, Gender); // It will print the firstName of 0th index and Gender of 2nd index.
+//------------------------------------------------------------------------------------------------------------
+/* Objects inside array --> Useful in Real-life Applications */
+const users = [
+  { userId: 1, firstName: "Shikhar", Gender: "Male" },
+  { userId: 2, firstName: "Prakhar", Gender: "Male" },
+  { userId: 3, firstName: "Harshit", Gender: "Male" },
+  { userId: 4, firstName: "Himanshu", Gender: "Male" },
+  { userId: 5, firstName: "Yash", Gender: "Male" },
+];
+
+// for (let user in users) {
+//   console.log(users);
+// }
