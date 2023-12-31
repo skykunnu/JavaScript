7377c24459078791138b3131d2ca5e98db564730
Shikhar Khandelwal <skykunnu@gmail.com>
2023-12-31 19:33:17 +0530

    new

--- DOM/dom.js
+++ DOM/dom.js
@@ -4,3 +4,23 @@
 //2. Changing HTML ----
 //3. Changing CSS ---
 //4. Event Listener ---
+
+// Program to Switch ON/Off a bulb.
+//--------------------------------------
+/* var bulb = document.querySelector("#bulb");
+var btn = document.querySelector("button");
+var Count = 0;
+btn.addEventListener("click", function () {
+  if (Count == 0) {
+    bulb.style.backgroundColor = "yellow";
+    console.log("Bulb is ON");
+    Count = 1;
+  } else {
+    bulb.style.backgroundColor = "transparent";
+    console.log("Bulb is Off");
+    Count = 0;
+  }
+}); */
+
+//-------------------------------------------------------------------------------------
+/* Selecting a multiple events at a same time. */
