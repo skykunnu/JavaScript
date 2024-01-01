30561650476a41795c39a7131d208169a9bcf81e
Shikhar Khandelwal <skykunnu@gmail.com>
2024-01-01 21:32:37 +0530

    new

---
+++ 02_Arrays_Objects/05_Functions.js
@@ -0,0 +1,12 @@
+// function singHappyBirthday() {
+//   console.log("Happy Birthday to you");
+// }
+// singHappyBirthday();
+//--------------------------------------------------------------------
+// function sumTwoNumbers(a, b) {
+//   return a + b;
+// }
+// const returnValue = sumTwoNumbers(4, 9);
+// console.log(returnValue);
+
+//------------------------------------------------------------------------
