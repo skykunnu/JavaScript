f6557811e4a91d83a266ec78e6b9baf8cc9078ae
Shikhar Khandelwal <skykunnu@gmail.com>
2024-01-01 21:57:19 +0530

    new

--- 02_Arrays_Objects/05_Functions.js
+++ 02_Arrays_Objects/05_Functions.js
@@ -1,19 +1,38 @@
-// function singHappyBirthday() {
-//   console.log("Happy Birthday to you");
-// }
-// singHappyBirthday();
-//--------------------------------------------------------------------
-// function sumTwoNumbers(a, b) {
-//   return a + b;
-// }
-// const returnValue = sumTwoNumbers(4, 9);
-// console.log(returnValue);
+/* Function Declaration */
 
-//------------------------------------------------------------------------
-function isEven(Number) {
-  if (Number % 2 === 0) {
-    return true;
-  }
-  return false;
+/* function singHappyBirthday() {
+  console.log("Happy Birthday to you");
 }
-console.log(isEven(11));
+singHappyBirthday();
+*/
+
+//-------------------------------------------------------------------------------------------------------------
+
+/* function sumTwoNumbers(a, b) {
+  return a + b;
+}
+const returnValue = sumTwoNumbers(4, 9);
+console.log(returnValue);
+*/
+//-------------------------------------------------------------------------------------------------------------
+
+/* Function to check whether a Num is Even or odd. */
+function isEven(Number) {
+  return Number % 2 === 0;
+}
+// console.log(isEven(13));
+
+//-----------------------------------------------------------------------------------------------------------
+function String(anyString) {
+  return anyString.slice(0, 1);
+}
+// console.log(String("Shikhar"));
+//------------------------------------------------------------------------------------------------------------
+
+/* Function Expression */
+
+const singHappyBirthday = function () {
+  console.log("Happy Birthday to you !!");
+};
+
+singHappyBirthday();
