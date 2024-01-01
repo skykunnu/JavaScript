a798b84aef1d9a810a9ddf5f12f95a01959892b6
Shikhar Khandelwal <skykunnu@gmail.com>
2024-01-01 22:11:56 +0530

    new

---
+++ 02_Arrays_Objects/06_ArrowFunctions.js
@@ -0,0 +1,9 @@
+/* Arrow Function */
+
+const singHappyBirthday = () => {
+  console.log("Happy Birthday to you !!");
+};
+// singHappyBirthday();
+
+const Sum = (a, b) => a + b; // function compressed into single line, first Sum function gets called after that values are assigned to parameters and then sum of a & b are return.
+console.log(Sum(2, 3));
