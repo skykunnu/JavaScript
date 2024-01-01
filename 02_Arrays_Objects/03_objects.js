60b3b9c586eb9b09548f56523374354db022239b
Shikhar Khandelwal <skykunnu@gmail.com>
2024-01-01 21:24:19 +0530

    new

--- 02_Arrays_Objects/03_objects.js
+++ 02_Arrays_Objects/03_objects.js
@@ -6,7 +6,7 @@ Objects- (Reference type or Non-Primitive)
 // -------------------------------------------------------------------------------------------------------------------
 // singleton
 
-//   1. whenever we declare object with literals then no singleton is formed.
+//   1. whenever we declare object with literals, then no singleton is formed.
 //   2. when declare with constructor then singleton is formed.
 //  object.create- declaration of object using constructor method.
 
