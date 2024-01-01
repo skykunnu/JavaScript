5ff44a08e55b0cfc0fde0b09bf7fb24da23b0022
Shikhar Khandelwal <skykunnu@gmail.com>
2024-01-02 00:23:39 +0530

    new

---
+++ 02_Arrays_Objects/08_Functions2.js
@@ -0,0 +1,29 @@
+// Functions inside function
+
+function app() {
+  const myFunc = () => {
+    console.log("hello from myFunc");
+  };
+  const sum = (num1, num2) => num1 + num2;
+  const mul = (num1, num2) => num1 * num2;
+  console.log("Inside app");
+  myFunc();
+  console.log(sum(2, 3));
+  console.log(mul(3, 3));
+}
+// app();
+//-------------------------------------------------------------
+
+// Lexical Scope
+function myApp() {
+  const myVar = "value1";
+
+  function myFunc() {
+    const myVar = "value59";
+    console.log("inside myFunc", myVar);
+  }
+
+  console.log(myVar);
+  myFunc();
+}
+myApp();
