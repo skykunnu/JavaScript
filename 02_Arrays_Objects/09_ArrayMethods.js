08634d8e89e7c3a15e9fe64c682615f96bc0eaff
Shikhar Khandelwal <skykunnu@gmail.com>
2024-01-02 16:28:34 +0530

    new

--- 02_Arrays_Objects/09_ArrayMethods.js
+++ 02_Arrays_Objects/09_ArrayMethods.js
@@ -1,5 +1,19 @@
 // Array Methods
-// Foreach
+
+// ForEach
 // Map
 // Filter
-// Reduce
+// reduce
+//--------------------------------------------------------------
+//1. ForEach
+const coding = ["JS", "Java", "Cpp", "Go", "Rust", "Python"];
+
+//Syntax for forEach Method using simple function.
+coding.forEach(function (Languages) {
+  console.log(Languages);
+});
+
+// using arrow function
+coding.forEach((Languages) => {
+  console.log(Languages);
+});
