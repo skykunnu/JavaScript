078912cad88fc1a01a6923f4ad9044ec3e5eef8b
Shikhar Khandelwal <skykunnu@gmail.com>
2024-01-02 12:35:11 +0530

    new

--- 02_Arrays_Objects/08_Functions2.js
+++ 02_Arrays_Objects/08_Functions2.js
@@ -61,5 +61,13 @@ function myFunc(a, b, ...c) {
 }
 // myFunc(3, 4, 5, 6, 7, 8);
 
-function addAll() {}
-addAll(2, 3, 4, 5, 6, 7, 8);
+function addAll(...numbers) {
+  /* rest parameter is used. */
+  let total = 0;
+  for (let number of numbers) {
+    total = total + number;
+  }
+  return total;
+}
+const ans = addAll(2, 3, 4, 5, 6, 7, 8);
+console.log(ans);
