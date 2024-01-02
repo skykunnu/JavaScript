299ff4a88ca652c8fc5b4ad91f22827f3a261340
Shikhar Khandelwal <skykunnu@gmail.com>
2024-01-03 00:09:28 +0530

    new

--- 02_Arrays_Objects/10_Methods.js
+++ 02_Arrays_Objects/10_Methods.js
@@ -9,3 +9,7 @@ number.sort();
 const userName = ["shikhar", "harshit", "yash", "prakhar"];
 userName.sort();
 console.log(userName); // [ 'harshit', 'prakhar', 'shikhar', 'yash' ] Sort according to alphabetical order.
+
+//------------------------------------------------------------------------------------------------------------------------
+
+// Find method
