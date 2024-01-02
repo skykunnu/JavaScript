973d54517666d9a2d188b90c9f4ed9a02376f269
Shikhar Khandelwal <skykunnu@gmail.com>
2024-01-02 21:52:15 +0530

    new

--- 02_Arrays_Objects/09_ArrayMethods.js
+++ 02_Arrays_Objects/09_ArrayMethods.js
@@ -5,7 +5,7 @@
 // Filter
 // reduce
 //-------------------------------------------------------------------------------------------------------------------
-//1. ForEach
+//1. ForEach- it does'nt return anything.
 const coding = ["JS", "Java", "Cpp", "Go", "Rust", "Python"];
 
 //Syntax for forEach Method using simple function.
@@ -38,3 +38,25 @@ Coding.forEach((item) => {
   // console.log(item.LanguageName); --> Printing the LanguageName of the objects defined inside the array.
 });
 //----------------------------------------------------------------------------------------------------------------------------------
+// Map method
+const numbers = [1, 2, 3, 4];
+
+/* Using Simple function*/
+// const square = function (number) {
+//   return number + number;
+// };
+
+/* Using arrow function*/
+
+const squareNumber = numbers.map((number, index) => {
+  // console.log(number * 2, index);
+}); // map method always return something in a new array.
+// console.log(squareNumber);
+
+//---------------------------------------------------------------------------------------------------------------
+
+// filter Method
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const NewNums = myNums.filter((num) => num > 4); // if you are not using curly braces then no need to write return num>4.
+console.log(NewNums);
