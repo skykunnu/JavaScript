4a7567c0ab35adcdb188fb9ef2935409fe2b774c
Shikhar Khandelwal <skykunnu@gmail.com>
2024-01-02 16:45:57 +0530

    new

--- 02_Arrays_Objects/09_ArrayMethods.js
+++ 02_Arrays_Objects/09_ArrayMethods.js
@@ -16,5 +16,24 @@ coding.forEach(function (Languages, index, Array) {
 // using arrow function
 coding.forEach((Languages, index, Array) => {
   /* In ForEach method we can pass index as well as whole array also. */
-  console.log(Languages, index, Array);
+  // console.log(Languages, index, Array);
+});
+
+const Coding = [
+  {
+    LanguageName: "JavaScript",
+    LanguageFileName: "js",
+  },
+  {
+    LanguageName: "CPP",
+    LanguageFileName: "cpp",
+  },
+  {
+    LanguageName: "GoLang",
+    LanguageFileName: "Go",
+  },
+];
+
+Coding.forEach((item) => {
+  // console.log(item.LanguageName); --> Printing the LanguageName of the objects defined inside the array.
 });
