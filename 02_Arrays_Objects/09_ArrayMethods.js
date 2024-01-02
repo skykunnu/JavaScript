c68990f2b9deb6b80997c215eb5e8d1a78d519be
Shikhar Khandelwal <skykunnu@gmail.com>
2024-01-02 16:32:53 +0530

    new

--- 02_Arrays_Objects/09_ArrayMethods.js
+++ 02_Arrays_Objects/09_ArrayMethods.js
@@ -9,11 +9,12 @@
 const coding = ["JS", "Java", "Cpp", "Go", "Rust", "Python"];
 
 //Syntax for forEach Method using simple function.
-coding.forEach(function (Languages) {
-  console.log(Languages);
+coding.forEach(function (Languages, index, Array) {
+  // console.log(Languages, index, Array);
 });
 
 // using arrow function
-coding.forEach((Languages) => {
-  console.log(Languages);
+coding.forEach((Languages, index, Array) => {
+  /* In ForEach method we can pass index as well as whole array also. */
+  console.log(Languages, index, Array);
 });
