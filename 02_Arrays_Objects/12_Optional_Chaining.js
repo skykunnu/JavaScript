cb869879225e310238fc28a31b5b15bbd592aa67
Shikhar Khandelwal <skykunnu@gmail.com>
2024-01-05 02:18:36 +0530

    new

--- 02_Arrays_Objects/12_Optional_Chaining.js
+++ 02_Arrays_Objects/12_Optional_Chaining.js
@@ -3,4 +3,6 @@ const user = {
   firstName: "Shikhar",
   // address: { houseNumber: "1234" },
 };
-console.log(user.address.houseNumber);
+// console.log(user.address.houseNumber); // this is giving error, because we have not defined it.
+
+console.log(user?.address?.houseNumber); // Read this statement like this, does user contains address or not. if true then proceed to next question or else return undefined.
