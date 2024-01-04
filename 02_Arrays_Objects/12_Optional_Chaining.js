aa858e80b6453112138db2ec694692a19f6d79c9
Shikhar Khandelwal <skykunnu@gmail.com>
2024-01-05 02:19:45 +0530

    new

--- 02_Arrays_Objects/12_Optional_Chaining.js
+++ 02_Arrays_Objects/12_Optional_Chaining.js
@@ -5,4 +5,4 @@ const user = {
 };
 // console.log(user.address.houseNumber); // this is giving error, because we have not defined it.
 
-console.log(user?.address?.houseNumber); // Read this statement like this, does user contains address or not. if true then proceed to next question or else return undefined.
+console.log(user?.address?.houseNumber); // Read this statement like this, does user contains address or not. if true then proceed to next question or else return undefined. --> this is giving undefined although we have not defined address.
