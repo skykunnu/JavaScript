03bd0415404860669f8622aa8b1d5271835bc449
Shikhar Khandelwal <skykunnu@gmail.com>
2024-01-05 02:45:47 +0530

    new

---
+++ 02_Arrays_Objects/13_This_Keyword.js
@@ -0,0 +1,26 @@
+// This Keyword- It basically points to the object to retrieve informations.
+
+function personInfo() {
+  console.log(`Person name is ${this.firstName} and person age is ${this.age}`);
+}
+
+const Person1 = {
+  firstName: "Shikhar",
+  age: 23,
+  about: personInfo,
+};
+
+const Person2 = {
+  firstName: "Yash",
+  age: 22,
+  about: personInfo,
+};
+
+const Person3 = {
+  firstName: "Harshit",
+  age: 22,
+  about: personInfo,
+};
+Person1.about(); // this keyword is pointing to Person1 obj
+Person2.about(); // this keyword is pointing to Person2 obj
+Person3.about(); // this keyword is pointing to Person3 obj
