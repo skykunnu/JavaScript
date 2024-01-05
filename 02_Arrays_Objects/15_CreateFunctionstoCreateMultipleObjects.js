5b5a1a59ccfbe8853ed7d78ce949c2d80edfac7b
Shikhar Khandelwal <skykunnu@gmail.com>
2024-01-06 00:43:40 +0530

    new

--- 02_Arrays_Objects/15_CreateFunctionstoCreateMultipleObjects.js
+++ 02_Arrays_Objects/15_CreateFunctionstoCreateMultipleObjects.js
@@ -1,8 +1,8 @@
-const user = {
+const user1 = {
   firstName: "Shikhar",
   LastName: "Khandelwal",
   email: "shikharkhandelwal27@gmail.com",
-  age: 2,
+  age: 22,
   address: "Triveni Nagar,Jaipur",
   about: function () {
     return `${this.firstName} is ${this.age} years old`;
@@ -11,3 +11,10 @@ const user = {
     return this.age >= 22;
   },
 };
+
+// But what if we want multiple users. To cater this we will create a function.
+/* 
+1. function (This function will create object )
+2. add key value pair
+3. then it will return the object.
+*/
