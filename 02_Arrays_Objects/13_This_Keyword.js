5b1c9b4e4f3b66dc1e91c5720b0598634fd4bc6b
Shikhar Khandelwal <skykunnu@gmail.com>
2024-01-06 12:08:47 +0530

    new

--- 02_Arrays_Objects/13_This_Keyword.js
+++ 02_Arrays_Objects/13_This_Keyword.js
@@ -51,15 +51,15 @@ const person = {
       console.log(this); // However, because checkThisAgain is a regular function (not a method of an object) and is not called as a method on an object, the 'this' inside it refers to the global object (Window in a browser environment). or // it will give undefined, becoz when we use strict keyword it will remove the default context (Windows).
     }
     // checkThisAgain();
-    console.log(this.name); // Undefined, because here 'this' is pointing to person object not the window object. inside person object no name is defined. or If we use self instead of this keyword, the output will be Shikhar. as self is capturing the value of this from the outer function, where it still refer to the person object.
+    // console.log(this.name); // Undefined, because here 'this' is pointing to person object not the window object. inside person object no name is defined. or If we use self instead of this keyword, the output will be Shikhar. as self is capturing the value of this from the outer function, where it still refer to the person object.
 
-    console.log(name); // Shikhar, because this.name is defined inside the window object. or when use self , output will be shikhar.
+    // console.log(name); // Shikhar, because this.name is defined inside the window object. or when use self , output will be shikhar.
   },
 };
-//person.checkThis(); // pointing to the object itself becoz callie of the method is person, which is an object.
-//console.log(person); // pointing to the object itself
+// person.checkThis(); // pointing to the object itself becoz callie of the method is person, which is an object.
+console.log(person); // pointing to the (Person object) itself
 
 const func = person.checkThis;
-func(); // As there is no calling context by default the output will be window object.
+// func(); // As there is no calling context by default the output will be window object.
 
 // console.log(person.checkThis());
