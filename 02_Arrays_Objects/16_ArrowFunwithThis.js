7efccab4358aed54c7477ea67484e6b450073486
Shikhar Khandelwal <skykunnu@gmail.com>
2024-01-07 18:24:21 +0530

    new

--- 02_Arrays_Objects/16_ArrowFunwithThis.js
+++ 02_Arrays_Objects/16_ArrowFunwithThis.js
@@ -1,1 +1,12 @@
 // Arrow function with this keyword
+/* In case of arrow function this keyword acts differently , means it points to outer environment object and outer environment object is Window object.  */
+const user = {
+  firstName: "Shikhar",
+  Age: 22,
+  about: () => {
+    console.log(this); // this will point to window object.
+    console.log(this.firstName, this.Age); // Undefined , Undefined.
+  },
+};
+
+user.about();
