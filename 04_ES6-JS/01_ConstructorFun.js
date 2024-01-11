fe872e9f2750fa3305e75eeab440c9ae7e54f7ac
Shikhar Khandelwal <skykunnu@gmail.com~>
2024-01-12 01:12:32 +0530

    new

--- 04_ES6-JS/01_ConstructorFun.js
+++ 04_ES6-JS/01_ConstructorFun.js
@@ -1,4 +1,6 @@
 // Constructor Function --> Important for interviews. (Allows you to make multiple instances from single object).
+
+
 /*They are used to create objects based on a template (in this case, the 'user' function). */
 /*This user function is designed to be used with the 'new' keyword,creating instances of the user object(like User1 & User2)*/
 
@@ -32,7 +34,10 @@ const User1= new user("Shikhar",12,true);
 const User2=new user("harshit",20,true);
 // console.log(User1.greeting());
 // console.log(User2);
+//--------------------------------------------------------------------------------------------------------------------------------------------------------
+// instanceof -This method is used to check whether an object belongs to particular class or constructor func. Return type is boolean.
+
 console.log(User1 instanceof user); // Obviously true , becoz object referenced by User1 is an instance of user function(as the empty object was created inside the user function). 
 console.log(User1 instanceof Object); // true,becoz object(Empty object) referenced by User1 is an instance of object and in JS all Objects inherit from the Object constructor. 
 
-// instanceof -This method is used to check whether an object belongs to particular class or constructor func. Return type is boolean.
+
