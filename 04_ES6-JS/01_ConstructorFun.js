4fa36f3d3f6bfeef02c39565622583050f359d58
Shikhar Khandelwal <skykunnu@gmail.com~>
2024-01-12 01:10:34 +0530

    new

--- 04_ES6-JS/01_ConstructorFun.js
+++ 04_ES6-JS/01_ConstructorFun.js
@@ -1,6 +1,6 @@
-// Constructor Function --> Important for interviews.
-/*They are used to create objecrs based on a template (in this case, the 'user' function). */
-/*This user function is designed to be used with the 'new' keyword,creating instances of the user object.*/
+// Constructor Function --> Important for interviews. (Allows you to make multiple instances from single object).
+/*They are used to create objects based on a template (in this case, the 'user' function). */
+/*This user function is designed to be used with the 'new' keyword,creating instances of the user object(like User1 & User2)*/
 
 function user(username,loginCount,isLoggedIn){
     /* here this keyword refers to the current instance of the object being created. when you create a new instance using 'new user()' 'this' points to that specific instance.  */
@@ -19,6 +19,14 @@ function user(username,loginCount,isLoggedIn){
 // but after initializing new user (User-2) and then taking log of User1 we found that values of user1 got overided by the User2. So therefore it is a big issue in itself.
 // to Avoid such overiding issues we have to make use of new keyword (Constructor function). 
 
+// new Keyword
+/* 1. Whenever you use 'new' keyword, an empty object is created which is called Instance.
+   2. A Constructor function is invoked with a new keyword. And an empty object is being created inside a constructor function (abstracted).
+   3. All 'this' keywords inside constructor func gets inject into an empty object. 
+   4. After Execution, 'new' keyword returns back whole object to the instances. 
+
+*/
+
 // Below are the two Instances of user function.
 const User1= new user("Shikhar",12,true);
 const User2=new user("harshit",20,true);
@@ -27,3 +35,4 @@ const User2=new user("harshit",20,true);
 console.log(User1 instanceof user); // Obviously true , becoz object referenced by User1 is an instance of user function(as the empty object was created inside the user function). 
 console.log(User1 instanceof Object); // true,becoz object(Empty object) referenced by User1 is an instance of object and in JS all Objects inherit from the Object constructor. 
 
+// instanceof -This method is used to check whether an object belongs to particular class or constructor func. Return type is boolean.
