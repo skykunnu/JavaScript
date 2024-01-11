80038434a2b0a4243f86fa441dfb45c1781e5e07
Shikhar Khandelwal <skykunnu@gmail.com~>
2024-01-11 20:01:31 +0530

    new

---
+++ 04_ES6-JS/03_ConstructorFun.js
@@ -0,0 +1,10 @@
+// Constructor Object. 
+// This Constructor function is acting like a class and s2 
+function Mobile(brand,price,launchDate){
+this.brand=brand;
+this.price=price;
+this.launchDate=launchDate;
+
+}
+const S2=new Mobile("Samsung","$250","2019");
+console.log(S2);
