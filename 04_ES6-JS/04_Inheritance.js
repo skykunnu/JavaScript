b593974a57d20997fae0acd0115c1ba33bc7031d
Shikhar Khandelwal <skykunnu@gmail.com>
2024-01-15 15:55:07 +0530

    new

--- 04_ES6-JS/04_Inheritance.js
+++ 04_ES6-JS/04_Inheritance.js
@@ -1,25 +1,33 @@
 class User{
-    constructor(username){
+    constructor(username,email,password){
         this.username=username;
+        this.email=email;
+        this.password=password;
     }
 
     logMe(){
-        console.log(`Username id ${this.username}`);
+        console.log(`Username id is  ${this.username}`);
     }
 
 }
+// The extends keyword is used to create a child class of another class(parent).
 class Teacher extends User{
     constructor(username,email,password){
-          super(username)
-          this.email=email
-          this.password=password
+/*The super() method refers to the parent class. 
+By calling the super() method in the constructor method, 
+we call the parent's constructor method and gets access to the parent's properties and methods. */
+        super(password)  
+        this.username=username
+        this.email=email
 
     }
-
+ 
     addCourse(){
+        super.logMe();
         console.log(`A new course was added by ${this.username}`);
     }
 }
 
 const chai=new Teacher("chai","chai@teacher.com","123")
+// console.log(chai.addCourse()); // 
 console.log(chai.addCourse());
