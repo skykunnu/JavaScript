ac13682aea8d5b996a9e370c6764f87d759da145
Shikhar Khandelwal <skykunnu@gmail.com>
2024-01-16 13:21:12 +0530

    new

--- 04_ES6-JS/06_Getters&Setters.js
+++ 04_ES6-JS/06_Getters&Setters.js
@@ -4,12 +4,13 @@ class Person{
         this.LastName=LastName;
         this.age=age;
     }
-    fullName(){ // 
+    get fullName(){ 
         return `${this.firstName} ${this.LastName}`;
     }
 }
 
 const person1=new Person("Shikhar","Khandelwal",23);
-console.log(person1.fullName()); // Shikhar khandelwal
+// console.log(person1.fullName()); // Shikhar khandelwal
+
 // To use fullname as property rather than method.
-console.log(person1.fullName);// this will return whole function.
+console.log(person1.fullName);// this will return whole function without using get and with get it will return Shikhar Khandelwal.
