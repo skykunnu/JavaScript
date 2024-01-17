4b872e0b235e11f02f3b1d70bb7b7a6eae26fa13
Shikhar Khandelwal <skykunnu@gmail.com>
2024-01-17 12:19:56 +0530

    new

---
+++ 03_DOM/index.js
@@ -0,0 +1,54 @@
+// DOM Manipulation
+
+
+// GetElementById()
+
+// const title=document.getElementById('main-heading');
+// console.log(title);
+
+// GetElementByClassName()
+
+// const list=document.getElementsByClassName('list-item')
+// console.log(list);
+
+// GetElementByTagName()
+
+// const listItem=document.getElementsByTagName('li');
+// console.log(listItem);
+
+// querySelector()
+
+// const container=document.querySelector('div');
+// console.log(container);
+
+
+// querySelectorAll()
+
+// const Container=document.querySelectorAll('div');
+// console.log(Container);
+
+//-------------------------------------------------------------------------------
+const title=document.querySelector('#main-heading');
+title.style.color='red';
+
+console.log(title);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
