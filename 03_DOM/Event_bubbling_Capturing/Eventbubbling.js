b651c496e6056c010cca8c26a9628939bb3d931b
Shikhar Khandelwal <skykunnu@gmail.com>
2024-02-10 22:04:03 +0530

    new

--- 03_DOM/Event_bubbling_Capturing/Eventbubbling.js
+++ 03_DOM/Event_bubbling_Capturing/Eventbubbling.js
@@ -1,31 +1,33 @@
-// Event bubbling/Propogation --> (Flow of Execution goes from child to parent).
-// Event capturing -->(Flow of Execution goes from parent to child)
+// Event bubbling/Propogation --> (Flow of Execution goes from child to parent).--> inside to outside
+// Event capturing -->(Flow of Execution goes from parent to child)--> outside to inside
 // Event delegation
 
 // stopPropagation--> it stops child to go to his parent element.
 // stopImmediatePropagation--> 
+
+
 const grandparent=document.querySelector(".grandparent");
-// const parent=document.querySelector(".parent");
-// const child=document.querySelector(".child");
+const parent=document.querySelector(".parent");
+const child=document.querySelector(".child");
 
 
 //Below is the concept of event bubbling.
 
-// child.addEventListener("click",(event)=>{
-    //    event.stopPropagation(); // it will restrict child to go to his parents.
-//     console.log("bubbling on child");
-// })
+child.addEventListener("click",(event)=>{
+    //    event.stopPropagation();   // it will restrict child to go to his parents.
+    console.log("bubbling on child");
+})
 
-// parent.addEventListener("click",()=>{
-//     console.log("bubbling on parent");
-// })
+parent.addEventListener("click",()=>{
+    console.log("bubbling on parent");
+})
 
-// grandparent.addEventListener("click",()=>{
-//     console.log("bubbling on grandparent");
-// })
-// document.body.addEventListener("click",()=>{
-//     console.log("bubbling on body");
-// })
+grandparent.addEventListener("click",()=>{
+    console.log("bubbling on grandparent");
+})
+document.body.addEventListener("click",()=>{
+    console.log("bubbling on document");
+})
 
 // Capturing Events--> (by making third argument of addEventListner as true.)
 
@@ -52,7 +54,8 @@ const grandparent=document.querySelector(".grandparent");
 this is because it will check on its parents whether any event is triggered or not but it found that event is triggered on grandparent.
 */
 grandparent.addEventListener("click",(e)=>{
-console.log(e.target.textContent); /* on clicking- child --> child
+// console.log(e.target.textContent);
+ /* on clicking- child --> child
  2. parent--> parent,child
  3. grandparent--> grandparent,parent,child*/
 })
