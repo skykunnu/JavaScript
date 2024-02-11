cb426b788a99a36b2c63b7625e0e467ca585f2b6
Shikhar Khandelwal <skykunnu@gmail.com>
2024-02-12 00:41:01 +0530

    new

--- 04_ES6-JS/SetInterval&Timeout/10_setInterval.js
+++ 04_ES6-JS/SetInterval&Timeout/10_setInterval.js
@@ -1,1 +1,15 @@
 
+const button=document.querySelector("#stop-btn");
+function showTime(){
+    const currentTime=new Date();
+    const time=`${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}`;
+    console.log(time);
+    document.getElementById("time").innerText=time;
+ }
+
+ const Interval=setInterval(showTime,1000); // calling showtime function after each sec.
+
+ // If we have to stop the setInterval function, then there is function called clearInterval().
+ button.addEventListener("click",()=>{
+ clearInterval(Interval);
+ });
