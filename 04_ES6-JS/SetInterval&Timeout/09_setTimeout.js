524c8fde3e543c2bb1dc7195a8628d9e17fe8847
Shikhar Khandelwal <skykunnu@gmail.com>
2024-02-11 09:57:13 +0530

    new

--- 04_ES6-JS/SetInterval&Timeout/09_setTimeout.js
+++ 04_ES6-JS/SetInterval&Timeout/09_setTimeout.js
@@ -4,11 +4,11 @@
 
 
 //synchronus programming --> one by one (first one task will be finished and then another task will be executed.)
-console.log("Script start");
-for(let i=1;i<10000;i++){
-    console.log("inside for loop");
-}
-console.log("Script end");
+// console.log("Script start");
+// for(let i=1;i<10000;i++){
+//     console.log("inside for loop");
+// }
+// console.log("Script end");
 
 
 // on Consoling above code,  the output is -->
@@ -19,15 +19,15 @@ Script end
 
 //Asynchronous Programming
 
-setTimeout
-console.log("script start");
+// setTimeout
+// console.log("script start");
 
 //setTimeout syntax
-const timeout=setTimeout(()=>{
-    console.log("hello");
-}, 1000); // (function you want to run, time after which you want to run the function(milisec))
-clearTimeout(timeout); // with this statement it will stop the execution of setTimeout function. 
-console.log("script end");
+// const timeout=setTimeout(()=>{
+//     console.log("hello");
+// }, 1000); // (function you want to run, time after which you want to run the function(milisec))
+// clearTimeout(timeout); // with this statement it will stop the execution of setTimeout function. 
+// console.log("script end");
 
 // On Consoling above code, the output-->
 /*
@@ -37,5 +37,21 @@ console.log("script end");
 */
 
 
+//-----------------------------------------------------------------------------------------------------------------
+ //clock
+
+ function showTime(){
+    const currentTime=new Date();
+    const time=`${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}`;
+    console.log(time);
+    document.getElementById("time").innerText=time;
+ }
+
+ setTimeout(()=>{
+    console.log("hi");
+ },5000); // After 5 sec it will print the value "hi" only once.
+ 
+
+ 
 
 
