7a70219a418c255680861be787d0c4da483b05b4
Shikhar Khandelwal <skykunnu@gmail.com>
2024-02-11 09:35:01 +0530

    new

--- 03_DOM/Event_bubbling_Capturing/Eventbubbling.js
+++ 03_DOM/Event_bubbling_Capturing/Eventbubbling.js
@@ -2,62 +2,73 @@
 // Event capturing -->(Flow of Execution goes from parent to child)--> outside to inside
 // Event delegation
 
-// stopPropagation--> it stops child to go to his parent element.
+// stopPropagation--> It prevents child to go to his parent element.
 // stopImmediatePropagation--> 
 
 
-const grandparent=document.querySelector(".grandparent");
-const parent=document.querySelector(".parent");
-const child=document.querySelector(".child");
+// const grandparent=document.querySelector(".grandparent");
+// const parent=document.querySelector(".parent");
+// const child=document.querySelector(".child");
 
 
 //Below is the concept of event bubbling.
 
-child.addEventListener("click",(event)=>{
-    //    event.stopPropagation();   // it will restrict child to go to his parents.
-    console.log("bubbling on child");
-})
+// child.addEventListener("click",(e)=>{
+//     // e.stopPropagation();// it will restrict child to go to his parents.
+//     console.log("bubbling on child");
+// })
 
-parent.addEventListener("click",()=>{
-    console.log("bubbling on parent");
-})
+// parent.addEventListener("click",(e)=>{
+//     console.log("bubbling on parent");
+// })
 
-grandparent.addEventListener("click",()=>{
-    console.log("bubbling on grandparent");
-})
-document.body.addEventListener("click",()=>{
-    console.log("bubbling on document");
-})
-
-// Capturing Events--> (by making third argument of addEventListner as true.)
+// grandparent.addEventListener("click",(e)=>{
+//     console.log("bubbling on grandparent");
+// })
+// document.body.addEventListener("click",(e)=>{
+//     console.log("bubbling on document");
+// })
 
 
 
-// child.addEventListener("click",()=>{
-//     console.log("Captured!!!!! on child");
-// },true)
 
-// parent.addEventListener("click",()=>{
-//     console.log("Captured!!!!! on parent");
-// },true)
+// Capturing Events--> (by making third argument of addEventListner as true., The One's which are true will printed out first and then the one on which the click event happened.)
 
-// grandparent.addEventListener("click",()=>{
-//     console.log("Captured!!!!! on grandparent");
-// },true)
+// if all elements are captured then the flow will be from outside to inside.
+
 // document.body.addEventListener("click",()=>{
-//     console.log("Captured!!!!! on body");
+//     console.log("Document capture");
+// },true)
+// grandparent.addEventListener("click",()=>{
+//     console.log("Grandparent capture");
+// },true)
+// parent.addEventListener("click",()=>{
+//     console.log("parent capture");
+// },true)
+// child.addEventListener("click",()=>{
+//     console.log("child capture");
 // },true)
 
 
 
-/*Event delegation--> It reduces the no of Event listners means that with single event listner you can call the remaining things associated with that element. for ex if you click on child the same output will be return as that of grandparent 
-this is because it will check on its parents whether any event is triggered or not but it found that event is triggered on grandparent.
-*/
-grandparent.addEventListener("click",(e)=>{
-// console.log(e.target.textContent);
- /* on clicking- child --> child
- 2. parent--> parent,child
- 3. grandparent--> grandparent,parent,child*/
-})
 
+
+/*Event delegation--> Event delegation is a design pattern in JavaScript where instead of attaching event listeners directly to individual elements,
+ you attach them to a parent element and use that parent element to listen for events that bubble up from its descendants.
+*/
+
+// it also works on the same concept as bubbling do ( from inside to outside)
+/* 1.) If we click on child --> 3 hi's will be printed out(child+parent+Grandparent).
+2.)  If we click on parent --> 2 hi's will be printed out (Parent + Grandparent).
+3.) If we click on Grandparent -->  1 hi will be printed out.
+
+*/
+
+const divs=document.querySelectorAll("div");
+
+divs.forEach(div => {
+    div.addEventListener("click",()=>{
+        console.log("hi");
+    })
+});
 
