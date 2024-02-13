e313ff7e7ba9cc3afdd892b8a701b79044830cac
Shikhar Khandelwal <skykunnu@gmail.com>
2024-02-13 18:13:08 +0530

    new

---
+++ 04_ES6-JS/13_AJAX.js
@@ -0,0 +1,18 @@
+//AJAX--> Asynchronous JavaScript And XML.
+/*
+1. It is a set of web development techniques.
+2. It is used to create asynchronous web applications.
+3. using Ajax, web applications can send and retrieve data from a server asynchronously(in the background)
+without interfering with the display and behaviour of the existing page. 
+4. We don't use data in XML format anymore but we use JSON now. 
+
+# We have 3 most common ways to create and send request to server
+1. XML HTTP Request (Old way of doing)
+2. Fetch API (new way of doing)
+3. axios (this is third party library)
+
+//HTTP request--> medium to talk between client and server.
+
+*/
+
+
