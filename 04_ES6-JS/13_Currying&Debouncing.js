da6b8d172e8bba4c8b25e06c307596d1b695d070
Shikhar Khandelwal <skykunnu@gmail.com>
2024-03-08 00:40:48 +0530

    new

--- 04_ES6-JS/13_Currying&Debouncing.js
+++ 04_ES6-JS/13_Currying&Debouncing.js
@@ -1,3 +1,22 @@
 // Currying
 
 
+// function Addition(a,b,c){
+//     return a+b+c;
+// }
+// let res=Addition(2,3,4);
+// console.log(res);
+
+
+// Currying function
+function Add(a){
+    return function(b){
+        return function(c){
+            return a+b+c; // this will not execute unless all the parameters are present. 
+        }
+    }
+}
+console.log(Add(2)(3)(4));
+
+
+// Practical Example
