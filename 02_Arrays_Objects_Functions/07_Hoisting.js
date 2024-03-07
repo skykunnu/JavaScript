155a882d41eccc15a3c11850ed828430856efbb9
Shikhar Khandelwal <skykunnu@gmail.com>
2024-03-07 12:39:27 +0530

    new

--- 02_Arrays_Objects_Functions/07_Hoisting.js
+++ 02_Arrays_Objects_Functions/07_Hoisting.js
@@ -1,6 +1,9 @@
-// Hoisting- When function call is done before initializing the function.
+// Hoisting- When a function call is done before initializing the function.
 /* Only possible in case of Function Declaration, Not possible in case of function Expression & Arrow functions */
-hello();
+
+hello(); // Hello world--> Because, before the invokation of hello function all the log values inside the function was already present in the global object. 
+console.log(x); // Undefined-->(Although variable x is defined in the memory space but it not defined with any value).
+var x=6;
 function hello() {
   console.log("Hello World");
 }
