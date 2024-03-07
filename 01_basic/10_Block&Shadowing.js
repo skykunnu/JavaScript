8a640fe5391123a2f71077c498cc7689134bfb15
Shikhar Khandelwal <skykunnu@gmail.com>
2024-03-07 12:45:40 +0530

    new

---
+++ 01_basic/10_Block&Shadowing.js
@@ -0,0 +1,7 @@
+// Block--> It is nothing buts a combining multiple javascript statements into one. 
+// We group multiple statement together in a block so that we can use it where js expects one statement. 
+{
+// Compound Statement
+var a=10;
+console.log(a);
+}
