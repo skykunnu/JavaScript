35dbc44aae9ff5d51d123a2cc8b6838b4ecb7887
Shikhar Khandelwal <skykunnu@gmail.com>
2024-04-22 02:07:02 +0530

    init

--- 04_ES6-JS/08_Closures.js
+++ 04_ES6-JS/08_Closures.js
@@ -1,29 +1,37 @@
-//Closure--> A function bundled together with lexical scope is called Closure.
+//Closure--> It is a combination of a function bundled together(enclosed) with references to its surrounding state (the lexical environment)
+
+// In other words, a closure gives you access to an outer function's scope from an inner function.
 
 
 //Use cases-->
 /* 1. Module Design pattern
 2. Currying
-3. functon like once
-4. memoize
+3. functon like adder
+4. memoization
 5. maintaining state in async world
 6. setTimeouts
 7. Iterators
 */
 
-
-function x(){
-    var a=7;
-    function y(){
-        console.log(a);
+// Ex--> Adder function
+function adder(num){ // Outer function (surrounding state)
+    
+    
+    function add(b){ // Inner function (enclosed)---> Closure (It is a closure that has num value present in it. which clearly means that add function has access to outer function's scope).
+        console.log(num+b);
     }
-    a=100;
-    return y;
+    return add;
 }
-var z=x(); // it will return function y.
-z(); // As we invoke z as a function , we already know a function y is returned to it by x and hence, It will print 7 and here comes the closures into picture, so when the function is returned from another function they still maintains their lexical scope, they remember where they were actually present.
-// though  x no longer exist but still the y function remembers its lexical scope. 
-// if we change the value of a to 100. the output of z function will be 100. because the function y is referenced to variable a not to the value of a. 
+const addTo=adder(10); // This 10 is passed as an argument to adder function, 
+// then add function is executed and after that adder fn returns add fn back from where it has been called, and then addTo fn is invoked 
+// with an argument that replaces b with it's value and finally console the value (num+b) with whatever arguments passed to each one of them. 
+addTo(2); // 12 ans. 
+
+
+
+
+
+
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // Corner Cases
 
@@ -40,3 +48,4 @@ z(); // As we invoke z as a function , we already know a function y is returned 
 // }
 // z();
 
+//------------------------------------------------------------------------------------------------------------------------------------------
