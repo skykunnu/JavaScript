ed847181bea91d25e19261e245876e2e01c719d4
Shikhar Khandelwal <skykunnu@gmail.com>
2024-04-26 01:52:45 +0530

    init

--- 02_Arrays_Objects_Functions/03_objects.js
+++ 02_Arrays_Objects_Functions/03_objects.js
@@ -33,13 +33,43 @@ const Person = {
   ],
 };
 /* How to insert a value of a variable inside a object as a key */
-Person[key=email] = "shikhar@gmail.com"; // simply place the variable without quotes in square brackets.
+// Person[key=email] = "shikhar@gmail.com"; // simply place the variable without quotes in square brackets.
 // console.log(Person);
 // console.log(Person["Person hobbies"]); // you cannot access variables separated by space with dot notation,So use bracket notation.
 // console.log(Person.Person hobbies); // this will give you error. 
 
+//---------------------------------------------------------------------------------------------------------------------------------------------
+// Object.freeze() & Object.seal()
+
+// Object.seal()--> This function prevents object from further addition and deletion of properties, but you can change the value's of existing properties.
+// Object.freeze()--> This function prevents everything even the changing of existing values.
+
+// in keyword--> This keyword checks whether the required property is present or not. 
+
+const user={
+  firstName:"Shikhar",
+  lastName: "Khandelwal",
+  address:{
+    city:'Jaipur',
+    pincode: 302018,
+    state:"Rajasthan",
+    moreDetails:{
+      population: 30000000,
+      area:'787 sq km'
+    },
+  },
+  age:23,
+  isGraduate:false
+}
 
 
+// Object.seal(user); --> Only user object is sealed not address object.
+// Object.seal(user.address) --> Only address obejct is sealed not user.
+
+// Object.freeze(user) --> Only user object is freezed not nested object.
+// Object.freeze(user.address) --> Only address object is freezed. 
+// console.log('age' in user); // true
+// console.log('mobileNo' in user); // false
 
 
 
